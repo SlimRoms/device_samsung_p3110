82c997f520afe174e302280fc20dec97b5a19385
Andreas Blaesius <skate4life@gmx.de>
2015-05-20 22:08:37 +0200

    P31XX: Overlay rework
    Split Slim and AOSP specific modifications.
    
    Change-Id: I101c971d40da391daea19a0045ed591aa2108265

--- slim.mk
+++ slim.mk
@@ -29,6 +29,8 @@ $(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/p3110/full_p3110.mk)
 
+DEVICE_PACKAGE_OVERLAYS += device/samsung/p3100/overlay/slim-common
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p3110
 PRODUCT_NAME := slim_p3110
